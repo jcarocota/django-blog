12bed37030b3c6daa0d8b15fddbb6a662c4e3c85
Julio Caro <jcarocota@gmail.com>
2025-09-10 12:45:15 -0700

    Model completo

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from datetime import date
+from .models import Post
 
 # Create your views here.
 
@@ -71,19 +72,25 @@ def get_date(post):
 
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
+    #sorted_posts = sorted(all_posts, key=get_date)
+    #latest_posts = sorted_posts[-3:]
     return render(request, "blog/index.html", {
       "posts": latest_posts
     })
 
 def posts(request):
+    all_posts = Post.objects.all()
     return render(request, "blog/all-posts.html", {
       "all_posts": all_posts
     })
 
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
+    #identified_post = Post.objects.get(slug=slug)
+    #all_posts = Post.objects.all()
+    #identified_post = next(post for post in all_posts if post['slug'] == slug)
     return render(request, "blog/post-detail.html", {
-      "post": identified_post
+      "post": identified_post,
+      "post_tags": identified_post.tags.all()
     })
