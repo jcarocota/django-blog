188eee02945bc8e6f3b10ca87cbfa2e53dfe3056
Julio Caro <jcarocota@gmail.com>
2025-09-14 13:15:00 -0700

    Refactor code structure for improved readability and maintainability

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.starting_page, name="starting-page"), #localhost:8000/
-    path("posts/", views.posts, name="posts-page"),
-    path("posts/<slug:slug>/", views.post_detail, name="post-detail-page") #posts/mi_primer_post
+    path("", views.StartingPageView.as_view(), name="starting-page"), #localhost:8000/
+    path("posts/", views.AllPostsView.as_view(), name="posts-page"),
+    path("posts/<slug:slug>/", views.SinglePostView.as_view(), name="post-detail-page") #posts/mi_primer_post
 ]
